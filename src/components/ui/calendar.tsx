4fa7f3b2ed5488014406232ced6b984e4d579e56
vitororigens <vitorabreufjb@hotmail.com>
2025-02-10 23:07:07 -0300

    adiciona novas dependências do Radix UI e atualiza o componente Calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,9 +54,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
+      } as any}
       {...props}
     />
   );
