ecf0f9a446f682575832d1ff558a4591aeb103f8
vitororigens <vitorabreufjb@hotmail.com>
2025-03-25 11:13:18 -0300

    feat: atualiza configurações do Firebase no arquivo .env e implementa filtro de uid no hook useFirestoreCollection

--- src/hooks/useFirestoreCollection.ts
+++ src/hooks/useFirestoreCollection.ts
@@ -1,18 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import {useAuthContext } from "@/context/AuthContext";
 import { database } from "@/services/firebase";
 import { collection, onSnapshot, query, where, Query, CollectionReference } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
+
 const useFirestoreCollection = (collectionName: string, queryParams?: { field: string, operator: any, value: any }) => {
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const { user } = useAuthContext();
 
   useEffect(() => {
+    if (!user?.uid) {
+      setLoading(false);
+      return;
+    }
+
     const collectionRef: CollectionReference = collection(database, collectionName);
     let collectionQuery: Query = collectionRef;
 
+    // Sempre adiciona o filtro de uid
+    collectionQuery = query(collectionRef, where("uid", "==", user.uid));
+
+    // Adiciona filtros adicionais se existirem
     if (queryParams) {
-      collectionQuery = query(collectionRef, where(queryParams.field, queryParams.operator, queryParams.value));
+      collectionQuery = query(collectionQuery, where(queryParams.field, queryParams.operator, queryParams.value));
     }
 
     const unsubscribe = onSnapshot(collectionQuery, (snapshot) => {
@@ -26,7 +38,7 @@ const useFirestoreCollection = (collectionName: string, queryParams?: { field: s
     });
 
     return () => unsubscribe();
-  }, [collectionName, queryParams]);
+  }, [collectionName, queryParams, user?.uid]);
 
   return { data, loading };
 };
